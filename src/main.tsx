463e05471a990980ace14487430149e8d129bf73
imhidayatchitrali <hidayat.rehman64@gmail.com>
2025-06-05 22:18:33 +0500

    PWA fixes

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,19 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-// import { registerSW } from 'virtual:pwa-register'
+import { registerSW } from 'virtual:pwa-register'
+
 // main.tsx or App.tsx
 // import { registerSW } from 'virtual:pwa-register'
 
-// registerSW({
-//   onNeedRefresh() {
-//     console.log('New content available, refresh to update.')
-//   },
-//   onOfflineReady() {
-//     console.log('App ready to work offline.')
-//   }
-// })
+registerSW({
+  onNeedRefresh() {
+    console.log('New content available, refresh to update.')
+  },
+  onOfflineReady() {
+    console.log('App ready to work offline.')
+  }
+})
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
