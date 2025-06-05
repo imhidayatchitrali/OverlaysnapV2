20dd010e470caeaa2608b84856f94790f23231a9
imhidayatchitrali <hidayat.rehman64@gmail.com>
2025-06-05 23:02:47 +0500

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -26,12 +26,12 @@ export default defineConfig({
         theme_color: '#000000',
         icons: [
           {
-            src: 'images/logo-192x192.png',
+            src: '/images/logo-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'images/logo-512x512.png',
+            src: '/images/logo-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
